34e08d0456563760c56e0b8efd545a54bfdf4918
Bruno Tomaz <tomazcraft@gmail.com>
2024-07-16 11:13:03 -0300

    Customizei meu admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactRegister(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'email',
+    ordering = '-id',
+    list_per_page = 10
+    list_max_show_all = 200
+
