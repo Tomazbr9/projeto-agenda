c064089826e567ac1f7dcdf90523e26c3574a5b7
Bruno Tomaz <tomazcraft@gmail.com>
2024-07-20 10:43:52 -0300

    Criei urls dinamicas para os contatos

--- contact/admin.py
+++ contact/admin.py
@@ -3,10 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactRegister(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'email', 'category',
+    list_display = 'id', 'first_name', 'last_name', 'email', 'category', 'show'
     ordering = '-id',
     list_per_page = 10
     list_max_show_all = 200
+    list_editable = 'show',
 
 @admin.register(models.Category)
 class CategoryRegister(admin.ModelAdmin):
