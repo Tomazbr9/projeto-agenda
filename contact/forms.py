1ef2f166af7bce445e4525a5881ffe95da60c1ae
Bruno Tomaz <tomazcraft@gmail.com>
2024-07-31 10:53:26 -0300

    Criei a view delete

--- contact/forms.py
+++ contact/forms.py
@@ -27,6 +27,7 @@ class ContactForm(forms.ModelForm):
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
         )
         # widgets = {
         #     'first_name': forms.TextInput(
@@ -38,19 +39,27 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                "Primeiro nome não pode ser igual o segundo",
                 code='invalid'
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+            
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            raise ValidationError(
+                'Não digite ABC',
+                code='invalid'
+            )
+        
+        return first_name
--- contact/urls.py
+++ contact/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('<search/', views.search, name='search'),  # type:ignore
 
     # Contact (CRUD)
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
     path('contact/create/', views.create, name='create'),  # type:ignore
+    path('contact/<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('contact/<int:contact_id>/update', views.update, name='update'),  # type:ignore
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'), # type:ignore
 ]
